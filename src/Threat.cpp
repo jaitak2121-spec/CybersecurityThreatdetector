d5ce8667831a5e92d63079bf84abb9cd09dcd1b0
Jai Tak <jaitak2121@gmail.com>
2026-09-24 09:28:51 +0530

    Refactor Threat class to use 'this' pointer

--- src/Threat.cpp
+++ src/Threat.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-
+// Definition of static variable
 int Threat::threatCount = 0;
 
 
+// Constructor
 Threat::Threat(
     string id,
     string type,
@@ -15,54 +16,60 @@ Threat::Threat(
     string stat
 )
 {
-    threatId = id;
-    threatType = type;
-    sourceIp = ip;
-    severity = sev;
-    status = stat;
+    // Demonstration of this pointer
+    this->threatId = id;
+    this->threatType = type;
+    this->sourceIp = ip;
+    this->severity = sev;
+    this->status = stat;
 
     threatCount++;
 }
 
 
+// Getter functions
 string Threat::getThreatId() const
 {
-    return threatId;
+    return this->threatId;
 }
 
 string Threat::getThreatType() const
 {
-    return threatType;
+    return this->threatType;
 }
 
 int Threat::getSeverity() const
 {
-    return severity;
+    return this->severity;
 }
 
 
+// Display threat
 void Threat::displayThreat() const
 {
     cout << "\n--- Threat Detected ---" << endl;
-    cout << "Threat ID   : " << threatId << endl;
-    cout << "Threat Type : " << threatType << endl;
-    cout << "Source IP   : " << sourceIp << endl;
-    cout << "Severity    : " << severity << endl;
+
+    cout << "Threat ID   : " << this->threatId << endl;
+    cout << "Threat Type : " << this->threatType << endl;
+    cout << "Source IP   : " << this->sourceIp << endl;
+    cout << "Severity    : " << this->severity << endl;
 
     cout << "Risk Level  : "
-         << ThreatLevel::getLevel(severity)
+         << ThreatLevel::getLevel(this->severity)
          << endl;
 
-    cout << "Status      : " << status << endl;
+    cout << "Status      : " << this->status << endl;
 }
 
 
+// Static function
 int Threat::getThreatCount()
 {
     return threatCount;
 }
 
 
+// Friend operator > overloading
 bool operator>(
     const Threat& t1,
     const Threat& t2
@@ -72,22 +79,32 @@ bool operator>(
 }
 
 
+// Friend stream operator << overloading
 ostream& operator<<(
     ostream& out,
     const Threat& threat
 )
 {
     out << "\n--- Threat Details ---" << endl;
-    out << "Threat ID   : " << threat.threatId << endl;
-    out << "Threat Type : " << threat.threatType << endl;
-    out << "Source IP   : " << threat.sourceIp << endl;
-    out << "Severity    : " << threat.severity << endl;
+
+    out << "Threat ID   : "
+        << threat.threatId << endl;
+
+    out << "Threat Type : "
+        << threat.threatType << endl;
+
+    out << "Source IP   : "
+        << threat.sourceIp << endl;
+
+    out << "Severity    : "
+        << threat.severity << endl;
 
     out << "Risk Level  : "
         << Threat::ThreatLevel::getLevel(threat.severity)
         << endl;
 
-    out << "Status      : " << threat.status << endl;
+    out << "Status      : "
+        << threat.status << endl;
 
     return out;
 }
