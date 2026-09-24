d3922e7a916235356d99a9d2ecc0baf2ac78d3ed
Jai Tak <jaitak2121@gmail.com>
2026-09-24 09:29:54 +0530

    Update comments for clarity and consistency

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
 
 
     // -----------------------------------------
-    // 1. Create Login Event
+    // 1. Login Event
     // -----------------------------------------
 
     LoginEvent loginEvent(
@@ -32,7 +32,7 @@ int main()
 
 
     // -----------------------------------------
-    // 2. Create Network Event
+    // 2. Network Event
     // -----------------------------------------
 
     NetworkEvent networkEvent(
@@ -48,7 +48,7 @@ int main()
 
 
     // -----------------------------------------
-    // 3. Create Threat Detector
+    // 3. Threat Detector
     // -----------------------------------------
 
     ThreatDetector detector;
@@ -146,7 +146,7 @@ int main()
 
 
     // -----------------------------------------
-    // 9. Final
+    // 9. final Keyword
     // -----------------------------------------
 
     cout << "\n========================================" << endl;
@@ -157,6 +157,91 @@ int main()
     cout << "Therefore, another class cannot inherit from NetworkEvent."
          << endl;
 
+
+    // -----------------------------------------
+    // 10. this Pointer
+    // -----------------------------------------
+
+    cout << "\n========================================" << endl;
+    cout << " this Pointer" << endl;
+    cout << "========================================" << endl;
+
+    cout << "The this pointer refers to the current object." << endl;
+    cout << "It is used inside Threat and event constructors."
+         << endl;
+
+
+    // -----------------------------------------
+    // 11. Virtual Function
+    // -----------------------------------------
+
+    cout << "\n========================================" << endl;
+    cout << " Virtual Function" << endl;
+    cout << "========================================" << endl;
+
+    SecurityEvent* eventPtr = &loginEvent;
+
+    cout << "\nCalling displayEvent() using base pointer:"
+         << endl;
+
+    eventPtr->displayEvent();
+
+
+    // -----------------------------------------
+    // 12. Pointer to Object
+    // -----------------------------------------
+
+    cout << "\n========================================" << endl;
+    cout << " Pointer to Object" << endl;
+    cout << "========================================" << endl;
+
+    SecurityEvent* securityEventPtr = &networkEvent;
+
+    cout << "Calling NetworkEvent through SecurityEvent pointer:"
+         << endl;
+
+    securityEventPtr->displayEvent();
+
+
+    // -----------------------------------------
+    // 13. Pointer to Derived Class
+    // -----------------------------------------
+
+    cout << "\n========================================" << endl;
+    cout << " Pointer to Derived Class" << endl;
+    cout << "========================================" << endl;
+
+    LoginEvent* loginPtr = &loginEvent;
+
+    cout << "Calling LoginEvent using derived-class pointer:"
+         << endl;
+
+    loginPtr->displayEvent();
+
+
+    // -----------------------------------------
+    // 14. Virtual Base Class
+    // -----------------------------------------
+
+    cout << "\n========================================" << endl;
+    cout << " Virtual Base Class" << endl;
+    cout << "========================================" << endl;
+
+    cout << "EventInfo is used as a virtual base class"
+         << endl;
+
+    cout << "SecurityEvent inherits virtually from EventInfo."
+         << endl;
+
+    cout << "Source IP obtained through the virtual base:"
+         << endl;
+
+    cout << eventPtr->getSourceIp() << endl;
+
+
+    // -----------------------------------------
+    // 15. Simulation Completed
+    // -----------------------------------------
 
     cout << "\n========================================" << endl;
     cout << " Simulation Completed" << endl;
