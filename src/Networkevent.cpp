101caf8cbedafbb7743f8db8dfab15ac9431bace
Jai Tak <jaitak2121@gmail.com>
2026-09-24 09:27:58 +0530

    Fix member variable assignment and update display format

--- src/Networkevent.cpp
+++ src/Networkevent.cpp
@@ -13,17 +13,18 @@ NetworkEvent::NetworkEvent(
 )
     : SecurityEvent(id, "NETWORK_EVENT", time, ip, sev)
 {
-    destinationPort = port;
-    protocol = proto;
+    this->destinationPort = port;
+    this->protocol = proto;
 }
 
 void NetworkEvent::displayEvent() const
 {
     cout << "\n--- Network Event ---" << endl;
-    cout << "Event ID        : " << getEventId() << endl;
-    cout << "Event Type      : " << getEventType() << endl;
-    cout << "Source IP       : " << getSourceIp() << endl;
-    cout << "Protocol        : " << protocol << endl;
-    cout << "Destination Port: " << destinationPort << endl;
-    cout << "Severity        : " << getSeverity() << endl;
+
+    cout << "Event ID    : " << getEventId() << endl;
+    cout << "Event Type  : NETWORK_EVENT" << endl;
+    cout << "Source IP   : " << getSourceIp() << endl;
+    cout << "Severity    : " << getSeverity() << endl;
+    cout << "Destination : Port " << this->destinationPort << endl;
+    cout << "Protocol    : " << this->protocol << endl;
 }
